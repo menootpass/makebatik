cce37722921b2f225c5dc8f0fd609f9a8dcdc72f
menootpass <mrs.farhanary@gmail.com>
2026-07-03 10:01:24 +0700

    Meta pixel

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,6 +23,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id" className={`${inter.variable} ${playfair.variable}`}>
       <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1285354980084145');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=1285354980084145&ev=PageView&noscript=1" alt="Meta Pixel"/>
+        </noscript>
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
